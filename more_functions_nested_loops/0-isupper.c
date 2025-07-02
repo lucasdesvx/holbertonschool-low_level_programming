8fc57fe502ebf8c5ded064742bc6cbb6eccdb74b
lucasdesvx <lucasdesvaux2008@gmail.com>
2025-07-02 19:55:52 +0000

    m

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
+int _isupper(int c)
 {
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
+    if (c >= 'A' && c <= 'Z')
+        return (1);
     return (0);
 }
