18e015e07ba4cbebe66823c20368e0089085fdf8
lucasdesvx <lucasdesvaux2008@gmail.com>
2025-07-05 11:17:29 +0000

    m

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle using '#' followed by a new line
+ * @size: The size of the triangle
+ */
+void print_triangle(int size)
+{
+	int row, space, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		for (space = size - row; space > 0; space--)
+		{
+			_putchar(' ');
+		}
+		for (hash = 1; hash <= row; hash++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
