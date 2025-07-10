e8ee9eb81433686deb4872e37d5c2dc636c006f3
lucasdesvx <lucasdesvaux@gmail.com>
2025-07-10 19:24:44 +0000

    m

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,22 +10,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j;
-    int found;
+	unsigned int i, j;
+	int found;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        found = 0;
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                found = 1;
-                break;
-            }
-        }
-        if (!found)
-            break;
-    }
-    return (i);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		found = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+			break;
+	}
+	return (i);
 }
