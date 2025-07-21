137e9a9f8527c12ec5b2400667cf7a3dd79d0aa9
lucasdesvx <lucasdesvaux2008@gmail.com>
2025-07-21 15:18:32 +0000

    m

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: argument count
+ * @argv: array of argument strings
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
