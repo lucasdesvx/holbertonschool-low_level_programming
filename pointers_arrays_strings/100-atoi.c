24694d18c6b40488b03f057364e01bced1dcb573
lucasdesvx <lucasdesvaux2008@gmail.com>
2025-07-21 17:27:16 +0000

    m

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,23 @@
 #include "main.h"
 
 /**
- * _atoi - convertit une chaîne en entier.
+ * _atoi - convertit une chaîne en entier
  * @s: chaîne à convertir
  *
- * Return: entier converti, 0 si pas de chiffre trouvé
+ * Return: entier converti, 0 si pas de chiffre
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
+	int i = 0, sign = 1;
+	unsigned int result = 0;
 	int found_digit = 0;
 
-	/* Parcourir la chaîne */
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign = -sign;
 		else if (s[i] == '+')
-		{
-			/* signe inchangé */
-		}
+			;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			found_digit = 1;
@@ -33,12 +29,15 @@ int _atoi(char *s)
 	if (!found_digit)
 		return (0);
 
-	/* Conversion des chiffres */
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
-	return (sign * result);
+	/* Cas spécial INT_MIN */
+	if (sign == -1 && result > 2147483647)
+		return (-2147483648);
+
+	return (sign * (int)result);
 }
