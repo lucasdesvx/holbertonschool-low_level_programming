0a9ef069bae773d1b53a4277df4a0a235946919d
Your Name <you@example.com>
2025-10-14 07:02:14 +0000

    m

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,4 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-printf ("with proper grammar, but the outcome is a piece of art,"/n)
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
