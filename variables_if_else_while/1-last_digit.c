b1164e059b9efeda195b0d149a97086d9099b5fb
Your Name <you@example.com>
2025-10-15 17:13:18 +0000

    m

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#include <stdioi.h>
 
 /**
  * main - Entry point
@@ -14,14 +14,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	 if (n > 5)
-        printf("%d and is greater than 5\n", n);
-    else if (n == 0)
-        printf("%d and is 0\n", n);
-    else
-        printf("%d and is less than 6 and not 0\n", n);
-	
 
+	if (lastDigit > 5)
+		printf("and is greater than 5\n");
+	else if (lastDigit == 0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
