a5672242af34505a42fd4e2d2c21be1f1599e7a0
Your Name <you@example.com>
2025-10-16 09:02:00 +0000

    p

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ *
+ * return 
+ */
+
+int main(void)
+{
+	int alpha;
+	
+	for (alpha = "a"; alpha <= "z"; alpha++);
+	putchar(alpha)
+	for (alpha = "A"; alpha <= "Z"; alpha++);
+	putchar(alpha)
+		putchar(\n);
+	
+	return (0);
+{
