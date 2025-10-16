fa5aed36a5edcfe1cfc644cd4d731180ea04187a
Your Name <you@example.com>
2025-10-16 10:30:05 +0000

    m

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	char alpha;
-
+	
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	putchar(alpha);	
+		putchar(alpha);
 	for (alpha = 'A'; alpha <= 'Z'; alpha++)
 		putchar(alpha);
 	putchar('\n');
