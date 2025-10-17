ae7d68db3fde6d47fd873d78e3b4e29eb648e31b
Your Name <you@example.com>
2025-10-17 06:23:57 +0000

    m

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int  nombre;
+
+	for (nombre = 0; nombre > 10; nombre++)
+		putchar(nombre + '0');
+	putchar('\n');
+
+	return(0);
+}
