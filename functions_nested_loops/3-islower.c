b866c5fd7b801759e58c7cc02845f206b48b7120
Your Name <you@example.com>
2025-10-25 15:26:13 +0000

    m

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *islower - verifie juste si une lettre est minuscule
+ *
+ *return: 1 si cest une lettre minuscule
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return(1);
+	else
+		return(0);
+}
