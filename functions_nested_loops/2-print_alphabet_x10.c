5d0392674af677bc0ab48673a06b922460e55735
Your Name <you@example.com>
2025-10-25 10:17:22 +0000

    m

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,19 @@
 #include "main.h"
 
 /**
+ *print 10 time the alphabet
  *
- *
- *
+ *return void
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
+	char lettre;
+	int nombre;
+	
+	for (nombre = 0; nombre < 10; nombre++)
 
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+		_putchar(lettre);
+	_putchar('\n');
+}
