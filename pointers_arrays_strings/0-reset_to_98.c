b59aa5f170a4310a00eccbd98c55f49f31bfbf3b
Your Name <you@example.com>
2025-11-02 18:13:21 +0000

    m

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
+ * reset_to_98 - updates the value of an integer to 98
+ * @n: pointer to the integer to update
  *
- *
- *
- *
+ * Return: void
  */
 
 void reset_to_98(int *n)
