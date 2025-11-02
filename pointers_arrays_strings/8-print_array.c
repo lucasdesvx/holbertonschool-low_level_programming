8acae76accd92ae450f4562cae1c18629d49cceb
Your Name <you@example.com>
2025-11-02 18:02:39 +0000

    m

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int tableau;
+
+	for (tableau = 0; tableau < n; tableau++)
+	{
+		printf("%d", a[tableau]);
+		if (tableau != n - 1)
+			printf(" ,");
+	}
+	printf("\n");
+}
