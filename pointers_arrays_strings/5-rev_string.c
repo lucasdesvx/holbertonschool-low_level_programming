75a16c92483d1803a1d9270048ea13d4ddfc8492
Your Name <you@example.com>
2025-11-02 17:00:23 +0000

    m

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,4 +8,16 @@
 
 void rev_string(char *s)
 {
+	int a = 0, b;
+	char temp;
 
+	while (s[a] != '\0')
+		a++;
+
+	for (b = 0; b < a / 2; b++)
+	{
+		temp = s[b];
+		s[b] = s[a - b - 1];
+		s[a - b - 1] = temp;
+	}
+}
