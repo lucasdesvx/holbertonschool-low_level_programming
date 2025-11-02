2d2a72ab6671d0890d729a733e45b907a9ce58e4
Your Name <you@example.com>
2025-11-02 18:09:04 +0000

    m

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
+ * reset_to_98 - met a  jour la valeur d'un pointer
+ * @n: pointeur vers un entier
  *
- *
- *
- *
+ * Return: rien
  */
 
 void swap_int(int *a, int *b)
