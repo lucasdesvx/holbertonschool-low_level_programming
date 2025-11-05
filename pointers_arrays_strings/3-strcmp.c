7041cf00dd68718e7cb9a39bec59b4811bfc38cb
Your Name <you@example.com>
2025-11-05 17:20:13 +0000

    m

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,4 +10,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int cisfun = 0;
 
+
+	while (s1[cisfun] != '\0' && s2[cisfun] != '\0')
+	{
+		if (s1[cisfun] != s2[cisfun])
+		{
+			return (s1[cisfun] - s2[cisfun]);
+		}
+		cisfun++;
+	}
+	return (s1[cisfun] - s2[cisfun]);
+}
