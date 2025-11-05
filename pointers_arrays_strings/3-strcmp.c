41700df43551919ab775ecf0cce3172e427d89b7
Your Name <you@example.com>
2025-11-05 11:01:07 +0000

    m

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1:
+ * @s2:
+ *
+ * Return: to _strcmp
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reverse_array -  function that reverses the content of an array of integers
+ * @a:
+ * @n:
+ *
+ * Return: to reverse_array
+ */
+
+void reverse_array(int *a, int n)
+{
