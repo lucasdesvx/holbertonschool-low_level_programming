7adb0a766814579558550636d7d6925351f5cf85
Your Name <you@example.com>
2025-11-05 17:27:48 +0000

    m

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,11 +2,21 @@
 
 /**
  * reverse_array -  function that reverses the content of an array of integers
- * @a:
- * @n:
+ * @a: bla blah
+ * @n: bla blah
  *
  * Return: to reverse_array
  */
 
 void reverse_array(int *a, int n)
 {
+	int chiffre, temp;
+	
+	
+	for (chiffre = 0; chiffre < n / 2; chiffre++)
+	{
+		temp = a[chiffre];
+		a[chiffre] = a[n - 1 - chiffre];
+		a[n - 1 - chiffre] = temp;
+	}
+}
