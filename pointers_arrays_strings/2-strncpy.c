389adc705481d13fff26db066621ef43f4206512
Your Name <you@example.com>
2025-11-05 06:16:42 +0000

    m

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: destination
+ * @src: source
+ * @n: a variable
+ *
+ * Return: to -strncpy
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int copier;
+
+
+
+	for (copier = 0; copier < n && src[copier] != '\0'; copier++)
+	{
+		dest[copier] = src[copier];
+	}
+	for (; copier < n; copier++)
+	{
+		dest[copier] = '\0';
+	}
+
+
+	return (dest);
+}
