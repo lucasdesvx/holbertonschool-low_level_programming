43c64f8cb82314b76c5a68cc43a8401d43d353d0
Your Name <you@example.com>
2025-11-06 08:02:10 +0000

    m

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *string_toupper - function that changes all lowercase letters of a string to uppercase
+ * @
+ *
+ *
+ * return: to *string_toupper
+ */
+
+char *string_toupper(char *)
+{
+
