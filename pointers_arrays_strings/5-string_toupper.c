68568869905d5ec9c94ec6809a509debf81ec7d6
Your Name <you@example.com>
2025-11-06 08:36:44 +0000

    m

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,6 +8,15 @@
  * return: to *string_toupper
  */
 
-char *string_toupper(char *)
+char string_toupper(charstr)
 {
-
+	int look;
+	
+	
+	for (look = 0; str[look] != '\0'; look++)
+	{
+		if (str[look] >= 'a' && str[look] <= 'z')
+			str[look] = str[look] - 32;
+	}
+	return (str);
+}
