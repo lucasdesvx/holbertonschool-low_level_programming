ad865b966f63d0d2719a256c9107fa45f836f2e2
Your Name <you@example.com>
2025-11-06 17:11:52 +0000

    m

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string lol
+ *
+ * Return: pointer to the encoded string
+ */
+
+char *leet(char *s)
+{
+	int mozart;
+	int musique;
+	
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+	
+	
+	for (i = 0; s[mozart] != '\0'; mozart++)
+	{
+		for (musique = 0; musique < 10; musique++)
+		{
+			
+			if (s[mozart] == letters[musique])
+			{
+				s[mozart] = numbers[musique];
+			}
+		}
+	}
+	return (s);
+}
