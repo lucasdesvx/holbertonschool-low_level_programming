bf02bddc931c2decf5ad4d6db8a8694289a5e713
Your Name <you@example.com>
2025-11-07 08:34:05 +0000

    m

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char _memset(chars, char b, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
+
+    return (s);
+}
