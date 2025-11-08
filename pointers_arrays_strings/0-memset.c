f74f3b519650d4b8cb3df499f34801aa49ea1d73
Your Name <you@example.com>
2025-11-08 09:14:23 +0000

    m

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _memset -  function that fills memory with a constant byte
+ * *_memset -  function that fills memory with a constant byte
  * @n:
  * @b:
  *
- * return: to _memset
+ * return: to *_memset
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int memoire;
-	
-	
+
+
 	for (memoire = 0; memoire < n; memoire++)
 	{
 		s[memoire] = b;
 	}
-	
+
 	return (s);
 }
