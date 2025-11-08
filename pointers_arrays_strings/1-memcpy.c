72b6cc88b17753c1725b8c6b41f69698ace5aa20
Your Name <you@example.com>
2025-11-08 09:57:31 +0000

    m

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char _memcpy(chardest, char *src, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        dest[i] = src[i];
+    }
+
+    return (dest);
+}
