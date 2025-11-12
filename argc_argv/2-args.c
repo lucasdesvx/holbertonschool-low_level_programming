637379d47bf17697094d03841df430ad624ff350
Your Name <you@example.com>
2025-11-12 07:01:35 +0000

    m

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: argumentation count
+ * @argv: argumentation vecteur
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int rec§ot;
+
+
