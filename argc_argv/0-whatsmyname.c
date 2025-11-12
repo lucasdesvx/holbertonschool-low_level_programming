6084ec96484ff419adc06648e8448c49a4cbd23c
Your Name <you@example.com>
2025-11-12 06:38:18 +0000

    m

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	
+	
+	return (0);
+}
