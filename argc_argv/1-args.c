803e41f64bb24a62eae360aa01e00bba36c028b1
Your Name <you@example.com>
2025-11-12 06:54:29 +0000

    m

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
- * @argc: l'argumentation compter
- * @argv: l'argumentation vecteur
+ * @argc: argument count
+ * @argv: argument vector
  *
  * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	(void)argv;
+	
+	printf("%d\n", argc - 1);
+
 
 
 	return (0);
