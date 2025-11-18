b83e41ad999b3f0edc97229d2b560e422d99acfc
Your Name <you@example.com>
2025-11-18 06:56:09 +0000

    m

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,23 +14,23 @@ char *_strdup(char *str)
 {
 	char *ledouble;
 	int memoire = 0, longueur = 0;
-	
+
 	if (str == NULL)
 		return (NULL);
-	
+
 	while (str[longueur])
         longueur++;
-	
-	
-	
+
+
+
 	ledouble = malloc(longueur + 1);
 	if (ledouble == NULL)
-		
-		return (NULL);
 	
+		return (NULL);
+
 	for (memoire = 0; memoire < longueur; memoire++)
 		ledouble[memoire] = str[memoire];
-	
+
 	ledouble[longueur] = '\0';
 	return (ledouble);
 }
