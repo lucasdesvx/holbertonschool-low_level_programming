4caba9acc727b7e946ecd96d33917729c77c3b29
Your Name <you@example.com>
2025-11-18 06:53:36 +0000

    m

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in memory
+ * @memoire: a variable
+ * @ledouble:a variable
+ * @longueur: a variable
+ *
+ * Return: to _strdup
+ */
+
+char *_strdup(char *str)
+{
+	char *ledouble;
+	int memoire = 0, longueur = 0;
+	
+	if (str == NULL)
+		return (NULL);
+	
+	while (str[longueur])
+        longueur++;
+	
+	
+	
+	ledouble = malloc(longueur + 1);
+	if (ledouble == NULL)
+		
+		return (NULL);
+	
+	for (memoire = 0; memoire < longueur; memoire++)
+		ledouble[memoire] = str[memoire];
+	
+	ledouble[longueur] = '\0';
+	return (ledouble);
+}
