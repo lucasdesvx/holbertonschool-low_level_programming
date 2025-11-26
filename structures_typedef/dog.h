e9841a4705fb7886a96b1c11bec4435aa6bc0f67
Your Name <you@example.com>
2025-11-26 06:53:37 +0000

    m

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef MY_DOG
 #define MY_DOG
 
+/**
+ * struct dog - with the following elements
+ * @name: a pointer
+ * @age: a variable
+ * @owner: a pointer
+ *
+ */
+
 struct dog
 {
 	char *name;
