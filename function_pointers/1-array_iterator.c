3701f81eb1957f6be6ebe1dd967e6b4a12f7882d
Your Name <you@example.com>
2025-12-01 07:29:28 +0000

    m

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - that executes a function given as a parameter on each element of an array
+ * @array: pointer
+ * @size: variable
+ * @action: pointer
+ *
+ * Return: to array_iterator
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t leparametre;
+
+	if (array == NULL || action == NULL)
+		return;
+
+
+	for (leparametre = 0; leparametre < size; leparametre++)
+		action(array[leparametre]);
+}
