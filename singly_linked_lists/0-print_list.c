38ceb96fab78a09cee9d83574bac704e3d6241e7
Your Name <you@example.com>
2025-12-10 05:31:31 +0000

    m

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_list - that prints all the elements of a list_t list
+ * @h: pointer
+ * @list_t: cant change this argument
+ *
+ * Return: to print_list
+ */
+
+size_t print_list(const list_t *h)
+{
