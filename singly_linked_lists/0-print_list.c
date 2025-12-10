3d7a4a983c5988ad120948fc69a3af12b224ee3b
Your Name <you@example.com>
2025-12-10 15:49:01 +0000

    m

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,14 +13,15 @@
 size_t print_list(const list_t *h)
 {
 	size_t compter = 0;
-	
-	
+
+
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
+
 
 		h = h->next;
 		compter++;
