97022111260d6e4f2fdfb19f007085a4656e0cbe
Your Name <you@example.com>
2025-12-11 05:56:09 +0000

    m

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - that returns the number of elements
+ * @h: pointer
+ *
+ * Return: to list_len
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t compter = 0;
+	
+	while (h)
+	{
+		compter++;
+		h = h->next;
+	}
+	return (compter);
+}
