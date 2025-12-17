8ead35c6dbdd0bf5cc8e1b57613376c13f1bbe8b
Your Name <you@example.com>
2025-12-17 16:01:02 +0000

    m

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,28 +4,45 @@
 #include <fcntl.h>
 
 /**
- * read_textfile - that read a text file prints it to the standard output
- * @filename: bla bla
- * @letters: bla bla
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file
+ * @letters: number of letters
  *
  * Return: to read_textfile
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *planB;
-	ssize_t longueur;
-	int planA;
+	int compteur;
+	ssize_t planA;
+	ssize_t planB;
+	char *fichier;
 
 	if (filename == NULL)
 		return (0);
-	
-	planA = open(filename, O_RDONLY);
-	if (planA == -1)
+
+
+	compteur = open(filename, O_RDONLY);
+	if (compteur == -1)
 		return (0);
 
-	planB = malloc(letters);
-	if (planB == NULL)
+	fichier = malloc(letters);
+	if (fichier == NULL)
+	{
+		close(compteur);
+		return (0);
+	}
+
+	planA = read(compteur, fichier, letters);
+	planB = write(STDOUT_FILENO, fichier, planA);
+
+
+	free(fichier);
+	close(compteur);
+
+
+	if (planA == -1 || planB != planA)
 		return (0);
 
-
+	return (planB);
+}
