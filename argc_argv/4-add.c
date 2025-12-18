0dec7df50015622b90daf0716e4402dddbf5788c
Your Name <you@example.com>
2025-12-18 15:50:32 +0000

    m

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,23 +11,25 @@
 
 int main(int argc, char *argv[])
 {
-    int planA = 0;
-    int planB = 0;
-    int nombreC = 0;
-
-    for (planA = 1; planA < argc; planA++)
-    {
-        for (planB = 0; argv[planA][planB]; planB++)
-        {
-            if (argv[planA][planB] < '0' || argv[planA][planB] > '9')
-            {
-                printf("Error\n");
-                return 1;
-            }
-        }
-        nombreC += atoi(argv[planA]);
-    }
-
-    printf("%d\n", nombreC);
-    return 0;
+	int planA = 0;
+	int planB = 0;
+	int nombreC = 0;
+	
+	
+	for (planA = 1; planA < argc; planA++)
+	{
+		for (planB = 0; argv[planA][planB]; planB++)
+		{
+			if (argv[planA][planB] < '0' || argv[planA][planB] > '9')
+			{
+			
+			
+				printf("Error\n");
+				return 1;
+			}
+		}
+		nombreC += atoi(argv[planA]);
+	}
+	printf("%d\n", nombreC);
+	return 0;
 }
