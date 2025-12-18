3c79d9b5dc933e719654b2cd5f6bcd300c675721
Your Name <you@example.com>
2025-12-18 09:01:53 +0000

    m

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int planA = 0;
 	int planB = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 
@@ -26,8 +26,8 @@ int create_file(const char *filename, char *text_content)
 	planA = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (planA == -1)
 		return (-1);
-	
-	
+
+
 	if (write(planA, text_content, planB) == -1)
 	{
 		close(planA);
