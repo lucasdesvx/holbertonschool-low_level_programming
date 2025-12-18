3cfd2131e8047a8d16172deee965165f3de96fca
Your Name <you@example.com>
2025-12-18 09:00:26 +0000

    m

---
+++ file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * create_file - that creates a file who have a right access
+ * @filename:
+ * @text_content:
+ *
+ * Return: to create_file
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int planA = 0;
+	int planB = 0;
+	
+	if (filename == NULL)
+		return (-1);
+
+
+	while (text_content && text_content[planB])
+		planB++;
+
+	planA = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (planA == -1)
+		return (-1);
+	
+	
+	if (write(planA, text_content, planB) == -1)
+	{
+		close(planA);
+		return (-1);
+	}
+	close(planA);
+	return (1);
+}
